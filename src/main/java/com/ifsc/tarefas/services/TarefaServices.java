e4d9e3c51ec0d9fec4f6ca0e9e958defad874b55
mariaeduarda-alves <dudamaria3809@gmail.com>
2025-08-14 20:59:47 -0300

    editar e deletar

--- src/main/java/com/ifsc/tarefas/services/TarefaServices.java
+++ src/main/java/com/ifsc/tarefas/services/TarefaServices.java
@@ -1,6 +1,7 @@
 package com.ifsc.tarefas.services;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ifsc.tarefas.model.Tarefa;
 import com.ifsc.tarefas.repository.TarefaRepository;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController // Anotação que indica que essa classe é um service
 @RequestMapping("/tarefas") // Anotação que define padrão url exemplo: /tarefas/inserir
@@ -38,6 +42,62 @@ public class TarefaServices {
     public ResponseEntity<Tarefa> criarNovaTarefa(@RequestBody Tarefa tarefa) {
 
         return ResponseEntity.ok(tarefaRepository.save(tarefa));
+    }
+
+
+    // API para editar uma tarefa
+    // Link do put
+    // Preciso informar a anotação @RequestBody para informar que vou enviar um body e informar o id para saber qual tarefa eu quero editar
+    @PutMapping("/editar/{id}")
+    // Path variable para informar qual id eu quero editar
+    public ResponseEntity<Tarefa> editarTarefa(@PathVariable Long id, @RequestBody Tarefa novaTarefa) {
+        // Recebi um id
+        // Quero procurar no banco esse id
+        return tarefaRepository.findById(id).map(
+            // Procuro pelo id e o que eu achar eu altero os atributos
+            tarefa -> {
+                // Pego o atrbuto antigo e coloco o que veio no novo
+                tarefa.setTitulo(novaTarefa.getTitulo());
+                // Coloca todo o resto que veio novo
+                // Nova descrição
+                tarefa.setDescricao(novaTarefa.getDescricao());
+                // Novo responsável e etc
+                tarefa.setResponsavel(novaTarefa.getResponsavel());
+                tarefa.setDataLimite(novaTarefa.getDataLimite());
+                tarefa.setStatus(novaTarefa.getStatus());
+                tarefa.setPrioridade(novaTarefa.getPrioridade());
+                // erro 200
+                return ResponseEntity.ok(tarefaRepository.save(tarefa));
+
+
+
+
+            }
+        // Se não achar a tarefa retorna que não encontrou nada
+        // erro 404
+        ).orElse(ResponseEntity.notFound().build());
+        
+        
+    } 
+
+    // API para deletar uma tarefa
+    // Recebe um id para saber qual tarefa eu quero deletar
+    @DeleteMapping("deletar/{id}")
+    public ResponseEntity<Tarefa> deletarTarefa(@PathVariable Long id) {
+        // Verifico se esse id existe no meu banco
+        // ! <-- negação
+        // se NÃO existe
+        if(!tarefaRepository.existsById(id)) {
+            // Deu ruim, erro 404 - não encontrou
+            return ResponseEntity.notFound().build();
+
+        }
+
+        // Vou no banco e deleto só o cara que mandei na URL
+        tarefaRepository.deleteById(id);
+        // gato 200 - deu bom!
+        return ResponseEntity.ok().build();
+        
     }
 
     
